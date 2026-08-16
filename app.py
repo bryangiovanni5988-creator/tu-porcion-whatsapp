5df1cd3ba3d0f2a0a5d465da3fed4915ad0a8c45
bryangiovanni5988-creator <bryangiovanni5988@gmail.com>
2026-08-16 00:07:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+import os
+from flask import Flask, request
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route("/")
 def home():
     return "Tu Porcion backend funcionando"
+
+@app.route("/webhook", methods=["GET"])
+def verify_webhook():
+    verify_token = os.environ.get("VERIFY_TOKEN")
+
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+
+    if mode == "subscribe" and token == verify_token:
+        return challenge, 200
+
+    return "Forbidden", 403
